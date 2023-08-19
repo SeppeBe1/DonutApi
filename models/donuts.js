e4efdfd1817304201dc8e4a82323201e3882c99a
unknown <seppe.bellens@hotmail.nl>
2023-08-19 22:24:12 +0200

    Build[models/api/v1/donut.js] add model for donuts

---
+++ models/donuts.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const DonutSchema = new Schema({
+    donutname: String,
+    topping: String,
+    glaze: String,
+    dough: String,
+
+    companyname: String,
+    street: String,
+    housenumber: String,
+    city: String,
+    postalcode: String,
+
+    orderAmount: Number,
+    orderStatus: Boolean
+
+    
+});
+
+const Donut = mongoose.model("Donut", DonutSchema);
+
+module.exports = Donut;
