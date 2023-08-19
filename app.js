42a7a67379125dc303efd9a604ed2c46187b9338
unknown <seppe.bellens@hotmail.nl>
2023-08-19 22:13:22 +0200

    Build[app.js] add router

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var donutRouter = require('./routes/api/v1/donuts');
 
 var app = express();
 
@@ -19,6 +21,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors());
+app.use('/api/v1/donuts', donutRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
